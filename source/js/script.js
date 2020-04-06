1399b80da8ef377a97ecea2b4d4d9df65a1f994a
Frozenzxc <39233749+Frozenzxc@users.noreply.github.com>
2020-04-06 22:05:58 +0300

    Добавляет мобильную версию сайта

--- source/js/script.js
+++ source/js/script.js
@@ -1,44 +1,21 @@
-const sliderItems = document.querySelectorAll(`.clients__item`);
-const sliderButtons = document.querySelectorAll(`.clients__counter-btn`);
+const clientsTextOpenBtn = document.querySelector(`.clients__info-toggle`);
+const clientsText = document.querySelector(`.clients__text`);
+const clientsTextCloseBtn = document.querySelector(`.clients__text-toggle`);
+const clientsNav = document.querySelector(`.clients__nav-wrapper`);
 
-const slider = (slides, dots) => {
-  const currentSlide = document.querySelector(`.clients__item--active`);
-  const currentDot = document.querySelector(`.clients__counter-btn--active`);
-  let index = +currentDot.dataset.id;
-  index = index < dots.length - 1 ? index : -1;
-  currentSlide.classList.remove(`clients__item--active`);
-  currentDot.classList.remove(`clients__counter-btn--active`);
-
-  dots[index + 1].classList.add(`clients__counter-btn--active`);
-  slides[index + 1].classList.add(`clients__item--active`);
-};
+  clientsTextOpenBtn.addEventListener(`click`, (evt) => {
+  clientsText.style.display = `block`;
+  clientsTextOpenBtn.display = `none`;
+  clientsNav.style.left = `29px`;
+  clientsNav.style.right = ``;
+  clientsNav.style.bottom = `37px`;
 
 
-setInterval(() => slider(sliderItems, sliderButtons),2000);
-
-sliderButtons.forEach((btn) => {
-  btn.addEventListener(`click`, () => slider(sliderItems, sliderButtons))
-});
-
-const performersItems = document.querySelectorAll(`.performers__item`);
-const performersBtnPrev = document.querySelector(`.performers__nav-button--prev`);
-const performersBtnNext = document.querySelector(`.performers__nav-button--next`);
-const performersCounter = document.querySelector(`.performers__nav-counter`);
-
-performersBtnPrev.addEventListener(`click`,  () => {
-  const performersItemActive = document.querySelector(`.performers__item--active`);
-  let index = +performersItemActive.dataset.id;
-  performersItemActive.classList.remove(`performers__item--active`);
-  index = index === 0 ? performersItems.length : index;
-  performersItems[index - 1].classList.add(`performers__item--active`);
-  performersCounter.textContent = `${index}/${performersItems.length}`;
-});
-
-performersBtnNext.addEventListener(`click`,  () => {
-  const performersItemActive = document.querySelector(`.performers__item--active`);
-  let index = +performersItemActive.dataset.id;
-  performersItemActive.classList.remove(`performers__item--active`);
-  index = index === performersItems.length - 1 ? -1 : index;
-  performersItems[index + 1].classList.add(`performers__item--active`);
-  performersCounter.textContent = `${index + 2}/${performersItems.length}`;
+  clientsTextCloseBtn.addEventListener(`click`, () => {
+    clientsText.style.display = `none`;
+    clientsTextOpenBtn.display = `block`;
+    clientsNav.style.left = ``;
+    clientsNav.style.right = `71px`;
+    clientsNav.style.bottom = `27px`;
+  });
 });
