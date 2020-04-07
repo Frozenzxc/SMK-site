18c49e1e64d5cd6310e966393e2898f9000c85d4
Frozenzxc <39233749+Frozenzxc@users.noreply.github.com>
2020-04-07 18:04:43 +0300

    Добавляет планшетную версию

--- docs/js/script.js
+++ docs/js/script.js
@@ -3,9 +3,9 @@ const clientsText = document.querySelector(`.clients__text`);
 const clientsTextCloseBtn = document.querySelector(`.clients__text-toggle`);
 const clientsNav = document.querySelector(`.clients__nav-wrapper`);
 
-  clientsTextOpenBtn.addEventListener(`click`, (evt) => {
+clientsTextOpenBtn.addEventListener(`click`, (evt) => {
   clientsText.style.display = `block`;
-  clientsTextOpenBtn.display = `none`;
+  clientsTextOpenBtn.style.display = `none`;
   clientsNav.style.left = `29px`;
   clientsNav.style.right = ``;
   clientsNav.style.bottom = `37px`;
@@ -13,9 +13,27 @@ const clientsNav = document.querySelector(`.clients__nav-wrapper`);
 
   clientsTextCloseBtn.addEventListener(`click`, () => {
     clientsText.style.display = `none`;
-    clientsTextOpenBtn.display = `block`;
+    clientsTextOpenBtn.style.display = `block`;
     clientsNav.style.left = ``;
     clientsNav.style.right = `71px`;
     clientsNav.style.bottom = `27px`;
   });
+});
+
+const visionCards = Array.from(document.querySelectorAll(`.vision__item`));
+
+visionCards.forEach((card) => {
+  card.addEventListener(`click`, (evt) => {
+    const activeCard = document.querySelector(`.vision__item--active`);
+    const currentCard = evt.target.closest('li');
+    const cardCloseBtn = currentCard.querySelector(`.vision__close-btn`);
+
+    activeCard && activeCard.classList.remove(`vision__item--active`);
+    currentCard.classList.add(`vision__item--active`);
+
+    cardCloseBtn.addEventListener(`click`, (e) => {
+      e.stopPropagation();
+      e.target.parentElement.classList.remove(`vision__item--active`);
+    })
+  })
 });
