977b3b114ccc87fb59fcd011506a6819edeb721d
Frozenzxc <39233749+Frozenzxc@users.noreply.github.com>
2021-02-01 13:30:46 +0300

    Вносит правки

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,4 +1,4 @@
-//burger menu scroll
+// burger menu scroll
 const burgerTrigger = document.querySelector(`.main-nav__toggle-trigger`);
 window.addEventListener(`scroll`, () => {
   if (scrollY > 1) {
@@ -8,47 +8,204 @@ window.addEventListener(`scroll`, () => {
   }
 });
 
-//services
+// services
+const ANIMATION_TIMEOUT = 1000;
+const SlidePosition = {
+  NEXT: `next`,
+  FOLLOW: `follow`,
+};
+
+const slideClickHandler = (index, position) => {
+  const activeSlide = document.querySelector(`.services__item--active`);
+  const nextSlide = document.querySelector(`.services__item--next`);
+  const followSlide = document.querySelector(`.services__item--follow`);
+
+  if (position === SlidePosition.NEXT) {
+    activeSlide.classList.add(`anim-active-out`);
+    nextSlide.classList.add(`anim-next-in`);
+    followSlide.classList.add(`anim-follow-move`);
+  } else {
+    activeSlide.classList.add(`anim-active-out`);
+    nextSlide.classList.add(`anim-next-out`);
+    followSlide.classList.add(`anim-follow-in`);
+  }
+
+  setTimeout(() => {
+    activeSlide.classList.remove(`anim-active-out`);
+    nextSlide.classList.remove(`anim-next-in`);
+    followSlide.classList.remove(`anim-follow-move`);
+    nextSlide.classList.remove(`anim-next-out`);
+    followSlide.classList.remove(`anim-follow-in`);
+    servicesSlider.slideTo(index);
+  }, ANIMATION_TIMEOUT);
+};
+
+const servicesSlider = new Swiper(`.services__wrapper`, {
+  init: false,
+  preventInteractionOnTransition: true,
+  initialSlide: 3,
+  slidesPerView: 3,
+  loop: false,
+  speed: 1000,
+  effect: `fade`,
+  fadeEffect: {
+    crossFade: true
+  },
+  slideClass: `services__item`,
+  slideActiveClass: `services__item--active`,
+});
+
+servicesSlider.on(`init`, () => {
+
+  const index = servicesSlider.activeIndex;
+  const nextIndex = index + 1 > servicesSlider.slides.length - 1 ? 0 : index + 1;
+  const followIndex = nextIndex + 1 > servicesSlider.slides.length - 1 ? 0 : nextIndex + 1;
+  const nextSlide = servicesSlider.slides[nextIndex];
+  const followSlide = servicesSlider.slides[followIndex];
+  nextSlide.classList.add(`services__item--next`);
+  followSlide.classList.add(`services__item--follow`);
+});
+
+servicesSlider.init();
+
+servicesSlider.on(`slideChange`, () => {
+  const index = servicesSlider.activeIndex;
+  const nextIndex = index + 1 > servicesSlider.slides.length - 1 ? 0 : index + 1;
+  const followIndex = nextIndex + 1 > servicesSlider.slides.length - 1 ? 0 : nextIndex + 1;
+  const currentNextSlide = document.querySelector(`.services__item--next`);
+  const currentFollowSlide = document.querySelector(`.services__item--follow`);
+
+  currentNextSlide.classList.remove(`services__item--next`);
+  currentFollowSlide.classList.remove(`services__item--follow`);
+
+  servicesSlider.slides[nextIndex].classList.add(`services__item--next`);
+  servicesSlider.slides[followIndex].classList.add(`services__item--follow`);
+});
+
+servicesSlider.on(`slideChangeTransitionStart`, () => {
+  const index = servicesSlider.activeIndex;
+
+  servicesSlider.slides[index].classList.add(`anim-active-in`);
+});
+
+servicesSlider.on(`click`, (evt) => {
+  const clickedSlide = evt.target.closest(`li`);
+  const index = servicesSlider.activeIndex;
+  const nextIndex = index + 1 > servicesSlider.slides.length - 1 ? 0 : index + 1;
+  const followIndex = nextIndex + 1 > servicesSlider.slides.length - 1 ? 0 : nextIndex + 1;
+
+  if (clickedSlide && clickedSlide.classList.contains(`services__item--next`)) {
+    slideClickHandler(nextIndex, SlidePosition.NEXT);
+  } else if (clickedSlide && clickedSlide.classList.contains(`services__item--follow`)) {
+    slideClickHandler(followIndex, SlidePosition.FOLLOW);
+  }
+});
+
+const servicesBtnNext = document.querySelector(`.services__nav-button--next`);
+servicesBtnNext.addEventListener(`click`, () => {
+  if (servicesSlider.activeIndex + 1 > servicesSlider.slides.length - 1) {
+    slideClickHandler(0, SlidePosition.NEXT);
+  } else {
+    slideClickHandler(servicesSlider.activeIndex + 1, SlidePosition.NEXT);
+  }
+});
 
 const servicesCards = [...document.querySelectorAll(`.services__item`)];
+const modalOverlay = document.querySelector(`.modal-overlay`);
 
 servicesCards.forEach((card) => {
   const cardModal = card.querySelector(`.services__modal`);
   const modalOpenBtn = card.querySelector(`.services__btn-more`);
   const modalCloseBtn = card.querySelector(`.services__modal-close-btn`);
-  const modalOverlay = card.querySelector(`.services__modal-overlay`);
+
 
   modalOpenBtn.addEventListener(`click`, (evt) => {
     evt.preventDefault();
     cardModal.classList.add(`services__modal--show`);
-    modalOverlay.classList.toggle(`modal-close`);
+    modalOverlay.classList.remove(`modal-close`);
+    servicesBtnNext.classList.add(`services__nav-button--disabled`);
+    servicesBtnNext.disabled = true;
 
     modalOverlay.addEventListener(`click`, () => {
-      modalOverlay.classList.toggle(`modal-close`);
+      modalOverlay.classList.add(`modal-close`);
       cardModal.classList.remove(`services__modal--show`);
+      servicesBtnNext.classList.remove(`services__nav-button--disabled`);
+      servicesBtnNext.disabled = false;
     });
 
     modalCloseBtn.addEventListener(`click`, () => {
-      modalOverlay.classList.toggle(`modal-close`);
+      modalOverlay.classList.add(`modal-close`);
       cardModal.classList.remove(`services__modal--show`);
+      servicesBtnNext.classList.remove(`services__nav-button--disabled`);
+      servicesBtnNext.disabled = false;
     });
   });
 });
 
-//preview input
+// preview input
 const previewInput = document.querySelector(`.preview__form-input`);
 previewInput.addEventListener(`change`, () => {
   document.querySelector(`.preview__form-placeholder`).style.display = `none`;
 });
 
+// vision cards
 
-//clients block text toggle
+const visionCards = [...document.querySelectorAll(`.vision__item`)];
+
+visionCards.forEach((card) => {
+  /* const cardModal = card.querySelector(`.clients__modal`);
+  const modalOpenBtn = card.querySelector(`.clients__text-link`);
+  const additionalOpenBtn = card.querySelector(`.clients__additional-link`);
+  const modalCloseBtn = card.querySelector(`.clients__modal-close-btn`);*/
+  const visionModalOverlay = document.querySelector(`.vision__modal-overlay`);
+  const cardCloseBtn = card.querySelector(`.vision__close-btn`);
+
+  card.addEventListener(`click`, (evt) => {
+    /*const currentActiveCard = document.querySelector(`.vision__item--active`);
+    if (currentActiveCard) {
+      currentActiveCard.classList.remove(`vision__item--active`);
+    }
+*/
+    if (evt.target !== cardCloseBtn) {
+      card.classList.add(`vision__item--active`);
+      visionModalOverlay.classList.remove(`modal-close`);
+    } else {
+      card.classList.remove(`vision__item--active`);
+      visionModalOverlay.classList.add(`modal-close`);
+    }
+
+    visionModalOverlay.addEventListener(`click`, () => {
+      visionModalOverlay.classList.add(`modal-close`);
+      card.classList.remove(`vision__item--active`);
+    });
+  });
+
+/*
+  additionalOpenBtn.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    cardModal.classList.add(`clients__modal--show`);
+    modalOverlay.classList.remove(`modal-close`);
+
+    modalOverlay.addEventListener(`click`, () => {
+      modalOverlay.classList.add(`modal-close`);
+      cardModal.classList.remove(`clients__modal--show`);
+    });
+
+    modalCloseBtn.addEventListener(`click`, () => {
+      modalOverlay.classList.add(`modal-close`);
+      cardModal.classList.remove(`clients__modal--show`);
+    });
+  });*/
+});
+
+
+// clients block text toggle
 const clientsTextOpenBtn = document.querySelector(`.clients__info-toggle`);
 const clientsText = document.querySelector(`.clients__text`);
 const clientsTextCloseBtn = document.querySelector(`.clients__text-toggle`);
 const clientsNav = document.querySelector(`.clients__nav-wrapper`);
 
-clientsTextOpenBtn.addEventListener(`click`, (evt) => {
+clientsTextOpenBtn.addEventListener(`click`, () => {
   clientsText.style.display = `block`;
   clientsTextOpenBtn.style.display = `none`;
   clientsNav.style.left = `29px`;
@@ -107,8 +264,8 @@ clientsCards.forEach((card) => {
   });
 });
 
-//clients block slider
-const clientsSlider = new Swiper (`.clients__wrapper`, {
+// clients block slider
+const clientsSlider = new Swiper(`.clients__wrapper`, {
   preventInteractionOnTransition: true,
   initialSlide: 0,
   slidesPerView: 1,
@@ -148,10 +305,10 @@ clientsSlider.on(`slideChangeTransitionStart`, () => {
   activeSlide.querySelector(`.clients__next-case`).style.animation = `clientsItemsIn 1.3s ease`;
 });
 
-//blog block slider
+// blog block slider
 const blogThumbs = new Swiper(`.blog__thumbs-container`, {
   centeredSlides: true,
-  direction: 'horizontal',
+  direction: `horizontal`,
   spaceBetween: 24,
   slidesPerView: `auto`,
   loop: true,
@@ -162,7 +319,7 @@ const blogThumbs = new Swiper(`.blog__thumbs-container`, {
   },
 });
 
-const blogSlider = new Swiper (`.blog__list-container`, {
+const blogSlider = new Swiper(`.blog__list-container`, {
   slidesPerView: 1,
   loop: true,
 
